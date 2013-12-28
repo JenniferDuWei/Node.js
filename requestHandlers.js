3e1279159a5d0ca5d770c1b9331536ef3cbc62b6
JenniferDuWei <blackcatd304@gmail.com>
2013-12-28 17:51:54 +0800

    Added new external module
    Added show upload functionality

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,8 @@
-var exec = require("child_process").exec;
+var exec = require("child_process").exec,
+	querystring = require("querystring"),
+	fs = require("fs");
 
-function start(response) {
+function dir(response, postData) {
 	console.log("Request handler 'start' was called.");
 
 	exec("dir", function (error, stdout, stderr){
@@ -10,12 +12,46 @@ function start(response) {
 	});
 }
 
-function upload(response) {
+function upload(response, postData) {
 	console.log("Request handler 'upload' was called.");
 	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello Upload");
+	response.write("You've sent: " + querystring.parse(postData).text);
 	response.end();
 }
 
+function start(response, postData) {
+	console.log("Request handler 'start' was called.");
+	var body = '<html>'+
+		'<head>'+
+		'<meta http-equiv="Content-Type" content="text/html; '+
+		'charset=UTF-8" />'+
+		'</head>'+
+		'<body>'+
+		'<form action="/upload" method="post">'+
+		'<input type="file" name="upload">' +
+		'<input type="submit" value="Upload file" />' +
+		'</form>'+
+		'</body>'+
+		'</html>';
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.write(body);
+	response.end();
+}
+
+function show(response, postData){
+	console.log("Request handler 'show' was called.");
+	fs.readFile("./testpic.jpg", "binary", function(error, file){
+		if(error){
+			response.writeHead(500,{"Content-Type": "text/plain"});
+			response.write(error + "\n");
+			response.end();
+		}else{
+			response.writeHead(200, {"Content-Type": "image/jpg"});
+			response.write(file, "binary");
+			response.end();
+		}
+	});
+}
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
